2d0e16dd5ff2fce0b4465194fc7b58dc26a1ef13
Eduard Bardají Puig <ebardaji@gmail.com>
2016-04-16 08:54:03 -0700

    Initial commit

---
+++ TechRun.js
@@ -0,0 +1,21 @@
+//Tech Run
+
+
+
+var hrr16 = {
+	name: 'Daniel Ramos',
+	name: 'Eric Smith',
+  name: 'Nick Randall',
+  name: 'Neekon Etemad',
+  name: 'Mike Cruz',
+  nam   '  son',
+  name: 'Eduard Bardaji',
+  name: 'Josh Summerhays',
+  name: 'Timothy Woo',
+  name: 'Chris Weaver'  ,
+  name  :'Leo'
+  name: 'I can type now - Gabe'
+
+
+
+}
